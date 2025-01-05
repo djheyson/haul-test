e761d58affc0bdce267e812176ba699c7a0e5e49
Djheyson Oliveira <djheyson.dev@gmail.com>
2025-01-05 09:58:59 -0300

    Change coverage threshold for now

--- apps/nest-api/app/jest.config.ts
+++ apps/nest-api/app/jest.config.ts
@@ -18,10 +18,10 @@ export default {
   coverageReporters: ['text', 'html', 'lcov'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 30,
+      functions: 30,
+      lines: 70,
+      statements: 70,
     },
   },
 };
